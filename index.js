c86d928477f8dbd19648ff38f2bf6298d5e4e3f0
bere abreo <berenicej2013@gmail.com>
2021-05-21 06:31:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ function theBeatlesPlay(musicians, instruments) {
   return arr;
 }
 
-function johnLennonFacts(array) {
-  while() {
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
 
+function johnLennonFacts(facts) {
+  while() {
+    facts[n] = "!!!";
   }
+  return facts;
 }
 
 function iLoveTheBeatles(array) {
