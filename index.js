a14192ecdf3c8f58474c5624fd64b1f8094dad1a
bere abreo <berenicej2013@gmail.com>
2021-05-21 06:32:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ function johnLennonFacts(facts) {
   return facts;
 }
 
-function iLoveTheBeatles(array) {
+function iLoveTheBeatles(x) {
+  var array = [];
+  do {
+    x = array.push("I love the Beatles!");
+    x++;
+  } while (x < 15); {
 
+  }
+  return array;
 }
