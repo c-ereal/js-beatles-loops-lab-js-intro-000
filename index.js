6aa4bce4347923aff717ff915bfe80771cc777ef
bere abreo <berenicej2013@gmail.com>
2021-05-21 06:24:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+var musicianString = ["John", "Paul", "George", "Ringo"];
+var musicianInstrument = ["guitar", "guitar", "bass", "drums"];
+function theBeatlesPlay(musicians, instruments) {
+  const arr = [];
+  for (let i = 0; 1 = musicians.length, 1 > i; i++) {
+    arr.push('${musicians[i]} plays ${instruments[i]}');
+  }
+  return arr;
+}
+
+function johnLennonFacts(array) {
+  while() {
+
+  }
+}
+
+function iLoveTheBeatles(array) {
+
+}
